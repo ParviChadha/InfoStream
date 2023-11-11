27697365bb1726cf4b2096966b0705b2dfaaa140
ParviChadha <parvichadha1@gmail.com>
2023-11-11 17:27:02 -0500

    background

--- src/homeScreen.tsx
+++ src/homeScreen.tsx
@@ -1,11 +1,9 @@
+import './style2.css';
+
 function App(){
-    const bodyStyle: React.CSSProperties = {
-        backgroundColor: '#F4EEFF',
-    };
     return (
-        <div style = {bodyStyle}>
-            <h1> InfoStream </h1>
-        </div>
+        <body style = {bodyStyle}>
+        </body>
     );
 }
 export default App;
