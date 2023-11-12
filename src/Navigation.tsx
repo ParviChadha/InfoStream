d68abcd0bfc7076d40b848bf2cfca39dfcbbf044
dann1m <dannieim689@gmail.com>
2023-11-11 23:44:30 -0500

    idek

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import "/src/App.css";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+<<<<<<< Updated upstream
 import HomeScreen from './pages/homescreen';
 import Business from './pages/business';
+=======
+import Business from './pages/business';
+import HomeScreen from './pages/homescreen';
+>>>>>>> Stashed changes
 
 const Navigation = () => {
     return (
@@ -12,6 +17,7 @@ const Navigation = () => {
             <Routes>
                 <Route path='/' element={<HomeScreen />} />
                 <Route path='/business' element={<Business />} />
+                
             </Routes>
         </Router>
     );
--- src/pages/business.tsx
+++ src/pages/business.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const Business = () => {
   return (
     <div>
@@ -8,3 +9,40 @@ const Business = () => {
 };
 
 export default Business;
+=======
+import React, { useState, useEffect } from 'react';
+
+const Business: React.FC = () => {
+    const [businessArticles, setBusinessArticles] = useState<any[]>([]);
+
+    useEffect(() => {
+        const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
+
+        fetch(`https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                setBusinessArticles(data.articles);
+            })
+            .catch(error => {
+                console.log('Error fetching business articles:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Business Page</h2>
+            <ul>
+                {businessArticles.map((article: any, index: number) => (
+                    <li key={index}>
+                        <h3>{article.title}</h3>
+                        <p>{article.description}</p>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Business;
+>>>>>>> Stashed changes
