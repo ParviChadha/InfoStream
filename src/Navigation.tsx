4311997d1fab7b70fdbfb4583d73f6111c277b66
dann1m <dannieim689@gmail.com>
2023-11-11 23:51:19 -0500

    idek

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import "/src/App.css";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-<<<<<<< Updated upstream
-import HomeScreen from './pages/homescreen';
-import Business from './pages/business';
-=======
 import Business from './pages/business';
 import HomeScreen from './pages/homescreen';
->>>>>>> Stashed changes
 
 const Navigation = () => {
     return (
         <Router>
             <Header />
             <Routes>
-                <Route path='/' element={<HomeScreen />} />
+                {/* <Route path='/' element={<HomeScreen />} */}
                 <Route path='/business' element={<Business />} />
-                
             </Routes>
         </Router>
     );
