002af4cefcf00e24165ca19e28aaba1a26bd9b80
dann1m <dannieim689@gmail.com>
2023-11-12 00:17:08 -0500

    all pages

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -4,6 +4,9 @@ import "/src/App.css";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Business from './pages/business';
 import HomeScreen from './pages/homescreen';
+import Entertainment from './pages/entertainment';
+import Technology from './pages/technology';
+import Science from './pages/science';
 
 const Navigation = () => {
     return (
@@ -12,6 +15,9 @@ const Navigation = () => {
             <Routes>
                 <Route path='/' element={<HomeScreen />} />
                 <Route path='/business' element={<Business />} />
+                <Route path='/entertainment' element={<Entertainment />} />
+                <Route path='/technology' element={<Technology />} />
+                <Route path='/science' element={<Science />} />
             </Routes>
         </Router>
     );
