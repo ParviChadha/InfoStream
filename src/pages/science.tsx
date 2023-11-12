a3c2d5131827cea5de770367e9325b7493dd515e
ParviChadha <parvichadha1@gmail.com>
2023-11-12 00:37:32 -0500

    topics_api_done

--- src/pages/science.tsx
+++ src/pages/science.tsx
@@ -1,10 +1,35 @@
-const Science = () => {
+import React, { useState, useEffect } from 'react';
+
+const Science: React.FC = () => {
+    const [scienceArticles, setScienceArticles] = useState<any[]>([]);
+
+    useEffect(() => {
+        const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
+
+        fetch(`https://newsapi.org/v2/top-headlines?category=science&country=us&apiKey=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                setScienceArticles(data.articles);
+            })
+            .catch(error => {
+                console.log('Error fetching science articles:', error);
+            });
+    }, []);
+
     return (
-      <div>
-        <h2>Science News</h2>
-        {/* Add your home page content here */}
-      </div>
+        <div>
+            <h2>Science Page</h2>
+            <ul>
+                {scienceArticles.map((article: any, index: number) => (
+                    <li key={index}>
+                        <h3>{article.title}</h3>
+                        <p>{article.description}</p>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  };
-  
-  export default Science;
+};
+
+export default Science;
--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -1,10 +1,35 @@
-const Technology = () => {
+import React, { useState, useEffect } from 'react';
+
+const Technology: React.FC = () => {
+    const [technologyArticles, setTechnologyArticles] = useState<any[]>([]);
+
+    useEffect(() => {
+        const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
+
+        fetch(`https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                setTechnologyArticles(data.articles);
+            })
+            .catch(error => {
+                console.log('Error fetching technology articles:', error);
+            });
+    }, []);
+
     return (
-      <div>
-        <h2>Technology News</h2>
-        {/* Add your home page content here */}
-      </div>
+        <div>
+            <h2>Technology Page</h2>
+            <ul>
+                {technologyArticles.map((article: any, index: number) => (
+                    <li key={index}>
+                        <h3>{article.title}</h3>
+                        <p>{article.description}</p>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  };
-  
-  export default Technology;
+};
+
+export default Technology;
