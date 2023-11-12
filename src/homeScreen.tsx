92e0290f52d96a280cffa82fc81f0b7efb743aee
ParviChadha <parvichadha1@gmail.com>
2023-11-11 19:20:56 -0500

    a;sjld

--- src/homeScreen.tsx
+++ src/homeScreen.tsx
@@ -22,7 +22,7 @@ function App() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "rgb(147, 86, 202)" }}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Infostream</a>
+          <a className="navbar-brand" href="#">InfoStream</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
