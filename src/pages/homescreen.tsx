07b1250ebcc962e576584a33aa4309be0fe87b9f
dann1m <dannieim689@gmail.com>
2023-11-12 00:05:55 -0500

    Update homescreen.tsx

--- src/pages/homescreen.tsx
+++ src/pages/homescreen.tsx
@@ -1,10 +1,31 @@
-const HomeScreen = () => {
+import React, { useState, useEffect } from 'react';
+
+const HomeScreen: React.FC = () => {
+    const [topHeadlines, setTopHeadlines] = useState<any[]>([]);
+    useEffect(() => {
+        const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                setTopHeadlines(data.articles);
+            })
+            .catch(error => {
+                console.log('Error fetching top headlines:', error);
+            });
+    }, []);
     return (
-      <div>
-        <h2>Top News:</h2>
-        {/* Add your home page content here */}
-      </div>
+        <div>
+            <h2>Top News:</h2>
+            <ul>
+                {topHeadlines.map((article: any, index: number) => (
+                    <li key={index}>
+                        <h3>{article.title}</h3>
+                        <p>{article.description}</p>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  };
-  
-  export default HomeScreen;
+};
+export default HomeScreen;
