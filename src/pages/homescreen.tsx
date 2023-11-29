ddeefb2d7e17132b268525b9f98882ef73cc382c
AryanKumar1401 <aryank1401@gmail.com>
2023-11-28 23:10:12 -0500

    storage v0

--- src/pages/homescreen.tsx
+++ src/pages/homescreen.tsx
@@ -1,38 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import '../style.css'
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../server/firebase-config';
+import Saved from '../pages/saved'; 
 
 
 const HomeScreen: React.FC = () => {
     const [topHeadlines, setTopHeadlines] = useState<any[]>([]);
+    const [savedArticles, setSavedArticles] = useState<any[]>([]);
+  
     useEffect(() => {
-        const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
-        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                setTopHeadlines(data.articles);
-            })
-            .catch(error => {
-                console.log('Error fetching top headlines:', error);
-            });
+      const apiKey = 'fc86f65ced7b44e9b86c02e971b9bdfc'; // Replace with your News API key
+      fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          setTopHeadlines(data.articles);
+        })
+        .catch(error => {
+          console.log('Error fetching top headlines:', error);
+        });
     }, []);
-
+  
+    const savePost = async (article) => {
+      try {
+        const usersCollectionRef = collection(db, 'users');
+        await addDoc(usersCollectionRef, {
+          title: article.title,
+          description: article.description,
+          url: article.url,
+        }).then(() => {
+          console.log('Article saved successfully!');
+          setSavedArticles(prevSavedArticles => [...prevSavedArticles, article]);
+        });
+      } catch (error) {
+        console.error('Error saving article:', error);
+      }
+    };
     
-
+  
     return (
-        <div className='backGround'>
-            <h2>Top News:</h2>
-            <ul>
-                {topHeadlines.map((article: any, index: number) => (
-                    <li key={index}>
-                        <h3>{article.title}</h3>
-                        <p>{article.description}</p>
-                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
-                        <button id='addArticle'>Add to Collection</button>
-
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div className='backGround'>
+        <h2>Top News:</h2>
+        <ul>
+          {topHeadlines.map((article: any, index: number) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                Read more
+              </a>
+              <button onClick={() => savePost(article)}>Add to Collection</button>
+            </li>
+          ))}
+        </ul>
+        <Saved savedArticles={savedArticles} />
+      </div>
     );
-};
+  };
 export default HomeScreen;
--- src/pages/saved.tsx
+++ src/pages/saved.tsx
@@ -1,18 +1,31 @@
-import { useState, useEffect } from "react";
+import React from 'react';
 
-
-function Saved() {
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        
-    }, [])
-
-    return <div> 
-            <h1>Saved Articles</h1>
-
-        </div>
-
+interface SavedProps {
+  savedArticles: any[];
 }
+
+const Saved: React.FC<SavedProps> = ({ savedArticles }) => {
+  return (
+    <div>
+      <h1>Saved Articles</h1>
+      <ul>
+        {savedArticles && savedArticles.length > 0 ? (
+          savedArticles.map((article: any, index: number) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                Read more
+              </a>
+            </li>
+          ))
+        ) : (
+          <p>No saved articles yet.</p>
+        )}
+      </ul>
+     
+    </div>
+  );
+};
 
 export default Saved;
